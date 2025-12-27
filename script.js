c3a0d51cd92d041ee9a098fd54985fd8c0339f56
Shubham Kale <manisht3851j@gmail.com>
2025-12-27 18:06:53 +0530

    Add dark mode toggle featurr
    Added theme toggle functionality to switch between light and dark modes.

--- script.js
+++ script.js
@@ -74,3 +74,14 @@ function removeTransaction(id) {
   transactions.forEach(addTransactionDOM);
   updateValues();
 }
+const themeToggle = document.getElementById("themeToggle");
+
+themeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  themeToggle.textContent =
+    document.body.classList.contains("dark")
+      ? "☀️ Light Mode"
+      : "🌙 Dark Mode";
+});
+
