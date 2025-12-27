31f23e1be10ae6a1d2af2edfda1e35b97b0c35b6
Shubham Kale <manisht3851j@gmail.com>
2025-12-27 17:19:02 +0530

    Add initial files for expense tracker

---
+++ script.js
@@ -0,0 +1,54 @@
+const balance = document.getElementById("balance");
+const income = document.getElementById("income");
+const expense = document.getElementById("expense");
+const list = document.getElementById("list");
+const form = document.getElementById("transactionForm");
+const text = document.getElementById("text");
+const amount = document.getElementById("amount");
+
+let transactions = [];
+
+form.addEventListener("submit", addTransaction);
+
+function addTransaction(e) {
+  e.preventDefault();
+
+  const transaction = {
+    id: Date.now(),
+    text: text.value,
+    amount: +amount.value
+  };
+
+  transactions.push(transaction);
+  addTransactionDOM(transaction);
+  updateValues();
+
+  text.value = "";
+  amount.value = "";
+}
+
+function addTransactionDOM(transaction) {
+  const sign = transaction.amount < 0 ? "-" : "+";
+  const item = document.createElement("li");
+
+  item.classList.add(transaction.amount < 0 ? "minus" : "plus");
+
+  item.innerHTML = `
+    ${transaction.text}
+    <span>${sign}₹${Math.abs(transaction.amount)}</span>
+  `;
+
+  list.appendChild(item);
+}
+
+function updateValues() {
+  const amounts = transactions.map(t => t.amount);
+
+  const total = amounts.reduce((acc, item) => acc + item, 0);
+  const incomeTotal = amounts.filter(item => item > 0).reduce((acc, item) => acc + item, 0);
+  const expenseTotal = amounts.filter(item => item < 0).reduce((acc, item) => acc + item, 0);
+
+  balance.innerText = total;
+  income.innerText = incomeTotal;
+  expense.innerText = Math.abs(expenseTotal);
+}
